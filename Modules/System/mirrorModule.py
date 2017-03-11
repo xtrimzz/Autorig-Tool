b64323d3ced11bb34732e4c810f26217cd7acc87
xtrimzz <aoadegbite15@gmail.com>
2017-03-11 07:09:29 +0100

    mirroring UI part 4 completed

--- Modules/System/mirrorModule.py
+++ Modules/System/mirrorModule.py
@@ -80,4 +80,57 @@ class MirrorModule:
 		return moduleInst.canModuleBeMirrored()
 		
 	def mirrorModule_UI(self):
-		print self.modules
+		self.moduleNames = []
+		for module in self.modules:
+			self.moduleNames.append(module.partition("__")[2])
+			
+		self.sameMirrorSettingsForAll = False
+		if len(self.modules) > 1:
+			result = cmds.confirmDialog(title="Mirror Multiple Modules", message=str(len(self.modules)) + " modules selected for mirror. \nHow would you like to apply mirror settings?", button=["Same for All", "Individually","cancel"], defaultButton="Same for All", cancelButton="Cancel", dismissString="Cancel")
+			
+			if result == "Cancel":
+				return
+			
+			if result == "Same for All":
+				self.sameMirrorSettingsForAll = True
+				
+		self.UIElements = {}
+		
+		if cmds.window("mirrorModule_UI_window", exists=True):
+			cmds.deleteUI("mirrorModule_UI_window")
+			
+		windowWidth = 300
+		windowHeight = 400
+		self.UIElements["window"] = cmds.window("mirrorModule_UI_window", width=windowWidth, height=windowHeight, title="Mirror Modules(s)", sizeable=False)
+		
+		self.UIElements["scrollLayout"] = cmds.scrollLayout(hst=0)
+		self.UIElements["topColumnLayout"] = cmds.columnLayout(adj=True, rs=3)
+		
+		scrollWidth = windowWidth 
+    
+		mirrorPlane_textWidth = 160
+		mirrorPlane_columnWidth = (scrollWidth - mirrorPlane_textWidth)/3
+		self.UIElements["mirrorPlane_rowColumn"] = cmds.rowColumnLayout(nc=4,columnAttach=(1,"right",0), columnWidth=[(1,mirrorPlane_textWidth), (2, mirrorPlane_columnWidth),(3, mirrorPlane_columnWidth),(4 , mirrorPlane_columnWidth)])
+		
+		cmds.text(label="Mirror Plane:")
+		self.UIElements["mirrorPlane_radioCollection"] = cmds.radioCollection()
+		cmds.radioButton("XY", label="XY", select=False)
+		cmds.radioButton("YZ", label="YZ", select=True)
+		cmds.radioButton("XZ", label="XZ", select=False)
+		
+		cmds.setParent(self.UIElements["topColumnLayout"])
+		cmds.separator()
+		
+		cmds.text(label="Mirrored Name(s):")
+		
+		columnWidth = scrollWidth/2
+		self.UIElements["moduleName_rowColumn"] = cmds.rowColumnLayout(nc=2, columnAttach=(1, "right", 0),columnWidth=[(1, columnWidth),(2, columnWidth)])
+		for module in self.moduleNames:
+			cmds.text(label = module + " >> ")
+			self.UIElements["moduleName_"+module] = cmds.textField(enable=True, text= module + "_mirror")
+				
+		cmds.showWindow(self.UIElements["window"])
+		
+			
+			
+			
