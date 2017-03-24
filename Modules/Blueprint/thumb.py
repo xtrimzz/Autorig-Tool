bd8ce79a958dab99d3e77f5ba5b586faf1ffc7b4
xtrimzz <aoadegbite15@gmail.com>
2017-03-24 06:33:49 +0100

    thumb implemented

---
+++ Modules/Blueprint/thumb.py
@@ -0,0 +1,23 @@
+import os
+import maya.cmds as cmds
+import System.blueprint as blueprintMod
+#reload(blueprintMod)
+
+import Blueprint.finger as finger
+
+import System.utils as utils
+reload(utils)
+
+
+CLASS_NAME = "Thumb"
+
+TITLE = "Thumb"
+DESCRIPTION = "Create 4 joints, defining a finger. Ideal use: thumb"
+ICON = os.environ["RIGGING_TOOL_ROOT"] + "/Icons/_thumb.xpm"
+
+class Thumb(finger.Finger):
+
+	def __init__(self, userSpecifiedName, hookObject):
+		jointInfo = [["root_joint", [0.0,0.0,0.0]], ["knuckle_1_joint", [4.0,0.0,0.0]], ["knuckle_2_joint", [8.0,0.0,0.0]], ["end_joint", [12.0,0.0,0.0]]] 
+		
+		blueprintMod.Blueprint.__init__(self, CLASS_NAME, userSpecifiedName, jointInfo, hookObject)
